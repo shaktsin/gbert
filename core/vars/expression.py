02353c6ceabf6a4e45a18f80ff16cf73546e7e12
shakti <shakti.singh@samsclub.com>
2020-06-14 00:33:43 -0500

    done

---
+++ core/vars/expression.py
@@ -0,0 +1,18 @@
+class Expression:
+
+    def __init__(self, operand_one, operand_two, operator="+"):
+        self.operator = operator
+        self.operand_one = operand_one
+        self.operand_two = operand_two
+
+    def __str__(self):
+        return "{}(operator={}, operand_one={}, operand_two={})".format(self.__class__.__name__, self.operator, self.operand_one, self.operand_two)
+
+    def __repr__(self):
+        return "{}(operator={}, operand_one={}, operand_two={})".format(self.__class__.__name__, self.operator, self.operand_one, self.operand_two)
+
+    def __add__(self, other):
+        return Expression(self, other, "+")
+
+    def __radd__(self, other):
+        return Expression(other, self, "+")
